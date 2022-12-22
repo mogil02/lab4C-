9761ac03597375372ea460c02396fefea150fd71
Alex <lexa.24@inbox.ru>
2022-12-22 15:03:23 +0700

    Lab6_v1.1

--- lab4C++.cpp
+++ lab4C++.cpp
@@ -6,15 +6,20 @@ int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    client* cl = new client;
-    dercl* dcl = new dercl;
+    client* cl = new client(12, "Гаврилов А. Д.");
+    dercl* dcl = new dercl(22, "Бодриков А. В.", 'M', 22);
+    cout << "Вывод объектов классов client и dercl, созданных конструкторами:" << endl << "client: ";
+    cl->output();
+    cout << "\ndercl: ";
+    dcl->output();
+    cout << "\nВвод возраста и гендера с использованием шаблонов:" << endl;
     TempClass<int> age; //Использование шаблона.
     TempClass<char> gender;
     cout << "Введите возраст: ";
     dcl->set_age(age.set());
     cout << "Введите пол: ";
     dcl->set_gender(gender.set());
-    cout << "\nИспользование производного класса от класса client. Вывод объектов с помощью виртуальной функции" << endl << "client: ";
+    cout << "\nИспользование производного класса от класса client. Вывод объектов с помощью виртуальной функции.\nclient: ";
     cl->output();
     cout << endl << "dercl: ";
     cl = dcl;
